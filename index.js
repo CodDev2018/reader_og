8f79533ec014ace79b48e0f208127b35d598fc4d
Paulo Roberto Sampaio Bezerra <bezerra.paulo@gmail.com>
2019-03-23 12:23:05 -0400

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const readline = require('readline-sync');
+
+const url = readline.question("Qual a URL da página a ser processada?");
+
+console.log(url);
+
+console.log('Estamos processando a extração dos dados da página...');
