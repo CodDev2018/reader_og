383dd07b52eb2b8df3da641e0731ff2104c2a800
Paulo Roberto Sampaio Bezerra <bezerra.paulo@gmail.com>
2019-03-31 07:31:15 -0400

    Validando url e fazendo download da página

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const readline = require('readline-sync');
+const axios = require('axios');
+
+const expression = /((www\.|(http|https)+\:\/\/)[_.a-z0-9-]+\.[a-z0-9\/_:@=.+?,##%&~-]*[^.|\'|\# |!|\(|?|,| |>|<|;|\)])/gi;
+const regex = new RegExp(expression);
 
 const url = readline.question("Qual a URL da página a ser processada?");
 
-console.log(url);
-
-console.log('Estamos processando a extração dos dados da página...');
+if (url.match(regex)) {
+    console.log('Estamos processando a extração dos dados da página...');
+    axios.get(url, {responseType: 'text'})
+        .then(function (result) {
+            console.log(result)
+        })
+} else {
+    console.log('A url informada não é válida!')
+}
